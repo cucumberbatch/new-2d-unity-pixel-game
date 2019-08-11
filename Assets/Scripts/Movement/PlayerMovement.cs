72fc489ef0b71a74eba1280f0527f765072cb416
cucumberbatch <45414849+cucumberbatch@users.noreply.github.com>
2019-08-12 03:29:43 +0600

    Structured folders of project
    * structured project
    * added partial representation of Killian
    * added native movement scipt

---
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speedRatio = 1;
+    private Rigidbody2D Body;
+
+    void Start()
+    {
+        Body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            Body.velocity = Vector2.left * speedRatio;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            Body.velocity = Vector2.right * speedRatio;
+        }
+        else
+        {
+            Body.velocity = Vector2.zero;
+        }
+        
+    }
+}
